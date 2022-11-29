fab2ddb2a49dbcf6c089a99c2db29ceb0e337109
janAsus <jan.peregrim@student.tuke.sk>
2022-11-29 01:41:18 +0100

    print field in Canvas

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 import random
+import tkinter.font
 from tkinter import *
+
 
 
 def create_random_playground(size):
@@ -14,14 +16,25 @@ def create_random_playground(size):
             numbers = []
 
     print(field)
-    print_field(field)
+    print_field(field,size)
 
-def print_field(field):
+def print_field(field,size):
 
     top = Tk()
     top.geometry("500x500")
     # creating a simple canvas.
-    c = Canvas(top, bg="black", height="500", width=500)
+    c = Canvas(top, bg="white", height="500", width=500)
+
+    X_index=0
+    Y_index=0
+    print()
+    for i in range(0,size):
+        Y_index+=100
+        for j in range(0,size):
+            c.create_rectangle(20+X_index, 20+Y_index, 120+X_index, 120+Y_index,outline="black", fill="white", width=2)
+            c.create_text(70+X_index, 70+Y_index, text=field[i][j], font=tkinter.font.Font(size=25,family='Helvetica'))
+            X_index+=100
+        X_index = 0;
 
     c.pack()
     top.mainloop()
